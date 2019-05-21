8dd31f792263adfeaf7f9f8195449e990ec7c0da
raj singh <rajsingh@box.com>
2019-05-21 16:43:54 -0700

    Updating build.sh
    	modified:   build.sh

--- build.sh
+++ build.sh
@@ -2,17 +2,18 @@
 
 set -ex
 
-name=alert-generator
-godir=/tmp/go/src
-scratchdir=/tmp/scratch
+godir=/tmp/go/src/github.com
 
-mkdir -p $godir
+mkdir -p $godir/box-autoremediation
 export GOPATH=/tmp/go
 
-cd $godir/$name
+mv cmd $godir/box-autoremediation/
+mv pkg $godir/box-autoremediation/
+cd $godir/box-autoremediation
+mkdir bin
 go get ./...
 
 CGO_ENABLED=1 GOOS=linux go build -o bin/alert-generator cmd/alert_generator.go
 
 mkdir -p /git-root/build
-mv bin/$name /git-root/build
+mv bin/alert-generator /git-root/build
