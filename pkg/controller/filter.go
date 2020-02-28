4510ae408c39a97815cb86dacfa2bc8cc5d87876
Raj Singh <rajsingh@rajsingh.dev.box.net>
2020-02-27 16:36:22 -0800

    Only checking for conditions which are True

--- pkg/controller/filter.go
+++ pkg/controller/filter.go
@@ -4,39 +4,40 @@ import (
 	"regexp"
 	"strings"
 	"time"
-	v1 "k8s.io/api/core/v1"
-	"github.com/spf13/viper"
-	log "github.com/sirupsen/logrus"
+
 	"github.com/box-autoremediation/pkg/controller/types"
+	log "github.com/sirupsen/logrus"
+	"github.com/spf13/viper"
+	v1 "k8s.io/api/core/v1"
 )
 
 //Filter filters nodes based on labels, conditions and status
 func Filter(filterCh <-chan *v1.Node, labelch chan<- *v1.Node, alertCh chan<- types.Alert, conf *viper.Viper) {
 	for {
 		select {
-		case node := <-filterCh:	
-		labelExcludeFilterOK := labelExcludeFilter(node.GetLabels(), conf.Sub("label_filter"))
-		labelIncludeFilterOK := labelIncludeFilter(node.GetLabels(), conf.Sub("label_filter"))
-		conditions, nodeOK := conditionsFilter(node.Status.Conditions, node.Name, conf.Sub("condition"), conf.GetBool("node_status.include_not_ready"))
-		log.Debugf("Filter - %+v", conditions)
-		log.Debugf("Filter - %+v", node.GetLabels())
-		log.Debugf("Filter - %v , %v",labelExcludeFilterOK, labelIncludeFilterOK)
-		if labelExcludeFilterOK && labelIncludeFilterOK && nodeOK && len(conditions) > 0 {
-			for _, alert := range conditions {
-				log.Infof("Filter - Sending %v_%v to updater", alert.Node, alert.Condition)
-				alertCh <- alert
+		case node := <-filterCh:
+			labelExcludeFilterOK := labelExcludeFilter(node.GetLabels(), conf.Sub("label_filter"))
+			labelIncludeFilterOK := labelIncludeFilter(node.GetLabels(), conf.Sub("label_filter"))
+			conditions, nodeOK := conditionsFilter(node.Status.Conditions, node.Name, conf.Sub("condition"), conf.GetBool("node_status.include_not_ready"))
+			log.Debugf("Filter - %+v", conditions)
+			log.Debugf("Filter - %+v", node.GetLabels())
+			log.Debugf("Filter - %v , %v", labelExcludeFilterOK, labelIncludeFilterOK)
+			if labelExcludeFilterOK && labelIncludeFilterOK && nodeOK && len(conditions) > 0 {
+				for _, alert := range conditions {
+					log.Infof("Filter - Sending %v_%v to updater", alert.Node, alert.Condition)
+					alertCh <- alert
 				}
-			if conf.GetBool("SetNodeLabel") {
-				//log.Infof("Filter - Sending %v to labeller", node.Name)
-				labelch <- node
+				if conf.GetBool("SetNodeLabel") {
+					//log.Infof("Filter - Sending %v to labeller", node.Name)
+					labelch <- node
+				}
 			}
 		}
-		}
 	}
-} 
+}
 
-func labelExcludeFilter(labels map[string]string, labelFilter *viper.Viper) (bool) {
-	if  !labelFilter.IsSet("exclude.key"){
+func labelExcludeFilter(labels map[string]string, labelFilter *viper.Viper) bool {
+	if !labelFilter.IsSet("exclude.key") {
 		//No exclude labels defined, do not ignore node
 		return true
 	}
@@ -50,7 +51,7 @@ func labelExcludeFilter(labels map[string]string, labelFilter *viper.Viper) (boo
 					//Node has excluded key but included regex match value, do not ignore this node
 					return true
 				}
-				
+
 			}
 			//Node has excluded key but not included value, ignore this node
 			return false
@@ -60,8 +61,8 @@ func labelExcludeFilter(labels map[string]string, labelFilter *viper.Viper) (boo
 	return true
 }
 
-func labelIncludeFilter(labels map[string]string, labelFilter *viper.Viper) (bool) {
-	if !labelFilter.IsSet("include"){
+func labelIncludeFilter(labels map[string]string, labelFilter *viper.Viper) bool {
+	if !labelFilter.IsSet("include") {
 		//No include labels defined, do not ignore node
 		return true
 	}
@@ -69,7 +70,7 @@ func labelIncludeFilter(labels map[string]string, labelFilter *viper.Viper) (boo
 		if key == labelFilter.GetString("include.key") {
 			if labelFilter.IsSet("include.match_val") {
 				matched, _ := regexp.MatchString(labelFilter.GetString("include.match_val"), val)
-				if matched{
+				if matched {
 					//Node has the include key and matching value, do not ignore this node
 					return true
 				}
@@ -84,8 +85,8 @@ func labelIncludeFilter(labels map[string]string, labelFilter *viper.Viper) (boo
 	return false
 }
 
-//Get not ready nodes 
-func conditionsFilter(conditions []v1.NodeCondition, nodeName string, condFilter *viper.Viper,  inclNotReady bool) ([]types.Alert, bool) {
+//Get not ready nodes
+func conditionsFilter(conditions []v1.NodeCondition, nodeName string, condFilter *viper.Viper, inclNotReady bool) ([]types.Alert, bool) {
 	var includeNode bool
 	var buf []types.Alert
 	var item types.Alert
@@ -93,35 +94,35 @@ func conditionsFilter(conditions []v1.NodeCondition, nodeName string, condFilter
 	for _, condition := range conditions {
 		condLower := strings.ToLower(string(condition.Type))
 		matched, _ := regexp.MatchString(condFilter.GetString("options.match"), string(condition.Type))
-		if matched && condition.Status != "False" && time.Since(condition.LastHeartbeatTime.Time) < condFilter.GetDuration("options.interval") && condFilter.IsSet("name." + condLower){
+		if matched && condition.Status == "True" && time.Since(condition.LastHeartbeatTime.Time) < condFilter.GetDuration("options.interval") && condFilter.IsSet("name."+condLower) {
 			item.Node = nodeName
 			item.Condition = string(condition.Type)
 			item.Attr.Timestamp = condition.LastHeartbeatTime.Time
 			item.Attr.Params = condition.Message
-			item.Attr.Action = condFilter.GetString("name." + string(condition.Type) + ".action" )
-			item.Attr.SuccessWait = condFilter.GetString("name." + string(condition.Type) + ".success_wait" )
-			item.Attr.FailedRetry = condFilter.GetString("name." + string(condition.Type) + ".failed_retry" )
+			item.Attr.Action = condFilter.GetString("name." + string(condition.Type) + ".action")
+			item.Attr.SuccessWait = condFilter.GetString("name." + string(condition.Type) + ".success_wait")
+			item.Attr.FailedRetry = condFilter.GetString("name." + string(condition.Type) + ".failed_retry")
 			buf = append(buf, item)
 		} else if condition.Type == "Ready" {
-			if  inclNotReady{
+			if inclNotReady {
 				includeNode = true
 				continue
-			} else if condition.Status == "True"{
+			} else if condition.Status == "True" {
 				includeNode = true
 				continue
 			} else {
 				includeNode = false
 			}
-		} 
+		}
 	}
-	if len(buf) == 0 && inclNotReady && condFilter.IsSet("Name.Node-Not-Ready"){
-			//Only Not Ready nodes with no failing NPD checks
-			item.Node = nodeName
-			item.Condition = "Node-Not-Ready"
-			item.Attr.Action = condFilter.GetString("Name.Node-Not-Ready.Action" )
-			item.Attr.SuccessWait = condFilter.GetString("Name.Node-Not-Ready.SuccessWait" )
-			item.Attr.FailedRetry = condFilter.GetString("Name.Node-Not-Ready.FailedRetry" )
-			buf = append(buf, item)
+	if len(buf) == 0 && inclNotReady && condFilter.IsSet("Name.Node-Not-Ready") {
+		//Only Not Ready nodes with no failing NPD checks
+		item.Node = nodeName
+		item.Condition = "Node-Not-Ready"
+		item.Attr.Action = condFilter.GetString("Name.Node-Not-Ready.Action")
+		item.Attr.SuccessWait = condFilter.GetString("Name.Node-Not-Ready.SuccessWait")
+		item.Attr.FailedRetry = condFilter.GetString("Name.Node-Not-Ready.FailedRetry")
+		buf = append(buf, item)
 	}
-return buf, includeNode
+	return buf, includeNode
 }
